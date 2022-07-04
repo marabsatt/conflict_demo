ca20c138ffe6e56e83a06d380e0a9426d70bbc6c
marabsatt <mitchelrabsatt@gmail.com>
2022-07-04 17:02:09 -0400

    merge

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-
-
-main()
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 8ec4dae42b484a9467dd5b1072146b225a8f1fa7
 
 
 main()
